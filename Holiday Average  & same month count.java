e6c3938824cc60b5d44fc24812403cd7afa99ec7
Mohammad Ashikur Rahman <122194673+Ashik2418@users.noreply.github.com>
2024-08-29 18:10:38 +0600

    Holiday Average  & same month count.java

--- Holiday Average  & same month count.java
+++ Holiday Average  & same month count.java
@@ -1,1 +1,43 @@
 
+package com.mycompany.holiday;
+
+public class Holiday {
+    private String name;
+    private int day;
+    private String month;
+
+  
+    public Holiday(String name, int day, String month) {
+        this.name = name;
+        this.day = day;
+        this.month = month;
+    }
+
+  
+    public boolean inSameMonth(Holiday other) {
+        return this.month.equals(other.month);
+    }
+
+    
+    public static double avgDate(Holiday[] holidays) {
+        int totalDays = 0;
+        for (int i = 0; i < holidays.length; i++) {
+            totalDays += holidays[i].day;
+        }
+        return (double) totalDays / holidays.length;
+    }
+
+    public static void main(String[] args) {
+        Holiday h1 = new Holiday("New Year", 1, "August");
+        Holiday h2 = new Holiday("Bangabandhu", 15, "August");
+        Holiday h3 = new Holiday("Seikh Hasina", 5, "January");
+
+        Holiday[] holidays = {h1, h2, h3};
+       
+        System.out.println("Average Day: " + avgDate(holidays));
+
+        
+        System.out.println("Holiday 1 and 2: " + h1.inSameMonth(h2));
+        System.out.println("Hodiday 1 and 3 : " + h1.inSameMonth(h3));
+    }
+}
